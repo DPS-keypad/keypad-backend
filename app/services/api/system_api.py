83184a32580059e7a71ae5ae7513bef5e902c1db
GledianMeta <g.meta@studenti.poliba.it>
2024-07-26 03:09:46 +0200

    brightness funziona

--- app/services/api/system_api.py
+++ app/services/api/system_api.py
@@ -1,5 +1,7 @@
 # this script is used to execute actions on the PC
 import os
+import screen_brightness_control as sbc
+import ctypes
 
 """ 
 def volume(volume_value):
@@ -9,16 +11,14 @@ def volume(volume_value):
     print(f"Volume set to {volume_value}%.")
 """
 
-
-
-import ctypes
-
 # Define constants
 VOLUME_MIN = 0
 VOLUME_MAX = 65535
 
 # Load the user32.dll library
 user32 = ctypes.windll.user32
+
+
 
 # Function to set the system volume
 def volume(volume_percentage):
@@ -43,21 +43,29 @@ def volume(volume_percentage):
     user32.SendMessageW(0xFFFF, 0x319, 0, (0xA0000 | (volume_value & 0xFFFF)))
 
 
+def brightness(brightness_value):
+    if brightness_value is None:
+        raise ValueError("Brightness value must be provided")
+
+    # Ensure the brightness value is within the range [0, 100]
+    try:
+        brightness_value = int(brightness_value)
+    except ValueError:
+        raise ValueError("Brightness value must be an integer")
+
+    if brightness_value < 0:
+        brightness_value = 0
+    elif brightness_value > 100:
+        brightness_value = 100
+
+    # Set the brightness to the specified value
+    sbc.set_brightness(brightness_value)
+    print(f"Brightness set to {brightness_value}%.")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+""" 
 def brightness(brightness_value):
     # Set the brightness to the specified value
     # TODO: Implement the brightness setting functionality
     print(f"Brightness set to {brightness_value}%.")
+"""
