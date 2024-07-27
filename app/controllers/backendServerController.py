f5378f98a5617c07c06cf5d7999d8a1757977669
salvatoregesualdo <salvigesualdo@libero.it>
2024-07-27 11:39:15 +0200

    fix cors (SERIAL DISABLED)

--- app/controllers/backendServerController.py
+++ app/controllers/backendServerController.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import app.controllers.actionController as actionController
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})
 
 @app.route('/api_list', methods=['GET'])
 def get_api_list():
@@ -12,12 +13,14 @@ def get_api_list():
     It returns a JSON response with the data received in the query parameters.
     """
     # Fetches the API list from the actionController
+    print("Received GET request")
     api_list = actionController.get_action_list()
     return jsonify({"data": api_list}), 200
 
 
 @app.route('/set_api', methods=['POST'])
 def post_set_api():
+    print("Received POST request")
     data = request.json
     key = data['key']
     action = data['action']
@@ -26,4 +29,5 @@ def post_set_api():
 
 
 def listen():
+    print("Server started")
     app.run(port=8000)
